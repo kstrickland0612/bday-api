23a8402275fa6b6faa7ee83b1db6c1192c94d0c3
kstrickland0612 <kstrickland0612@gmail.com>
2019-05-06 19:19:01 -0400

    Add notes about event patch bug to events_controller.rb
    bug does not affect front end functionality,
    so deprioritized

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -28,6 +28,31 @@ class EventsController < ProtectedController
   end
 
   # PATCH/PUT /events/1
+
+    # having an issue where an event can be patched to a friend owned by another user. This does not affect front-end functionality.
+
+    # Trying to fix this by checking to see if the friend_id in the update request is owned by the current user
+      # if not owned by the current user (returns nil), update all params except the friend_id so that friend_id never changes
+      # otherwise, business as usual
+        # event_params are updated
+        # or if there's an error, the status unprocessable entity is returned
+
+    # if this solution doesn't work, need to reorganize schema a bit so that events do not have user foreign key and so that events are a nested resource under friends.
+
+    # binding.pry
+
+  #   @friend = current_user.friends.find(event_params[:friend_id])
+  #   if @friend.nil?
+  #     if @event.update(event_params.except(:friend_id))
+  #       render json: @event
+  #     end
+  #   elsif @event.update(event_params)
+  #     render json: @event
+  #   else
+  #     render json: @event.errors, status: :unprocessable_entity
+  #   end
+  # end
+
   def update
     if @event.update(event_params)
       render json: @event
